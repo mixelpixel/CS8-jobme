0a1138862c147d46167d7c567ce2e389bdd30478
runranron <williamrpelton@gmail.com>
2018-07-23 10:27:04 -0500

    init

---
+++ server/tests/data/jobs/jobModel.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const chai = require('chai');
+const Job = require('../../../data/jobs/jobModel');
+
+const expect = chai.expect;
+
+describe('Database Tests', () => {
+  // Before starting the test, create a sandboxed database connection
+  // Once a connection is established invoke done()
+  before((done) => {
+    mongoose.connect('mongodb://localhost/local');
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error'));
+    db.once('open', () => {
+      console.log('We are connected to test database!');
+      done();
+    });
+  });
+  describe('Job should require titleAndSalary', () => {
+    const job = new Job({});
+  });
+});
